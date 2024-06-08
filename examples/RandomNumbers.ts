6a252410305657c87611faa421012246d309b5c5
ajitjha393 <ajitjha393@gmail.com>
2024-06-08 12:00:23 +0530

    add lazy random numbers usage example using `sthalit`

---
+++ examples/RandomNumbers.ts
@@ -0,0 +1,12 @@
+﻿import { Lazy } from '../src/index';
+
+const lazyRandomNumbers = (): Lazy<number> => {
+    return new Lazy<number>(function* () {
+        while (true) {
+            yield Math.random();
+        }
+    });
+};
+
+const firstFiveRandomNumbers = lazyRandomNumbers().take(5).toArray();
+console.log(firstFiveRandomNumbers); // [random values]
