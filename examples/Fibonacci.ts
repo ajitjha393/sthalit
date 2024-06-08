bb9d2d5a1e8d704057e435d041d0c958d7c4e113
ajitjha393 <ajitjha393@gmail.com>
2024-06-08 11:59:52 +0530

    add lazy fibonacci example using `sthalit`

---
+++ examples/Fibonacci.ts
@@ -0,0 +1,14 @@
+﻿import { Lazy } from '../src/index';
+
+const lazyFibonacci = (): Lazy<number> => {
+    return new Lazy<number>(function* () {
+        let a = 0, b = 1;
+        while (true) {
+            yield a;
+            [a, b] = [b, a + b];
+        }
+    });
+};
+
+const firstTenFibonacci = lazyFibonacci().take(10).toArray();
+console.log(firstTenFibonacci); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
