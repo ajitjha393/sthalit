f95c6acdf84d6c1f839e57996dea9c2fd295ac13
ajitjha393 <ajitjha393@gmail.com>
2024-06-08 11:17:25 +0530

    add tests for `sthalit`

---
+++ tests/Lazy.test.ts
@@ -0,0 +1,43 @@
+﻿import Lazy from '../src/core/Lazy';
+import { lazyRange, lazyRepeat } from '../src/core/Generators';
+
+describe('Lazy', () => {
+    it('should create a lazy sequence and take the first 5 elements', () => {
+        const sequence = new Lazy<number>(function* () {
+            let i = 0;
+            while (true) {
+                yield i++;
+            }
+        });
+
+        const result = sequence.take(5).toArray();
+        expect(result).toEqual([0, 1, 2, 3, 4]);
+    });
+
+    it('should map values correctly', () => {
+        const sequence = lazyRange(0).take(5).map(x => x * 2).toArray();
+        expect(sequence).toEqual([0, 2, 4, 6, 8]);
+    });
+
+    it('should filter values correctly', () => {
+        const sequence = lazyRange(0).take(10).filter(x => x % 2 === 0).toArray();
+        expect(sequence).toEqual([0, 2, 4, 6, 8]);
+    });
+
+    it('should flatMap values correctly', () => {
+        const sequence = lazyRange(0).take(3).flatMap(x => lazyRepeat(x, 2)).toArray();
+        expect(sequence).toEqual([0, 0, 1, 1, 2, 2]);
+    });
+
+    it('should reduce values correctly', () => {
+        const sum = lazyRange(0).take(5).reduce((acc, x) => acc + x, 0);
+        expect(sum).toEqual(10);
+    });
+
+    it('should zip sequences correctly', () => {
+        const seq1 = lazyRange(0).take(3);
+        const seq2 = lazyRepeat('a').take(3);
+        const zipped = seq1.zip(seq2).toArray();
+        expect(zipped).toEqual([[0, 'a'], [1, 'a'], [2, 'a']]);
+    });
+});
